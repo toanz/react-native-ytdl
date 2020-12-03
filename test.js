26622b364faf1551ee6d870836cde717d387cb0a
ToanZ <toan@ejoy.io>
2020-12-03 16:30:41 +0700

    finish report

--- test.js
+++ test.js
@@ -11,9 +11,15 @@ require("@babel/register")({
   });
 
 global.fetch = require('node-fetch');
+
+const utils = require('./lib/utils');
+
+// utils.checkForUpdates();
   
   // Import the rest of our application.
 const getLink = require('./index.js').default;
+
+const reportLog = require('./report.js').default;
 
 const getYoutubeLink = ytId => {
     if (!ytId) return '';
@@ -30,9 +36,34 @@ const getYoutubeLink = ytId => {
     console.log(urls)
 }
 
+// let arrLog = [];
+
+// function reportLog(...args) {
+//   arrLog = [ ...arrLog , ...args];
+//   console.log(...args);
+// }
+// const axios = require('axios');
+// function pushLog(data) {
+//   console.log(data)
+//   global.reportLog = null;
+//   return axios.post('http://192.168.1.14:1338/sheet/data', { 
+//     name: "bug",
+//     data: arrLog,
+//     key: "vuongtao"
+//   }).then(res => res.data);
+// }
+
+
+
+// global.reportLog = reportLog;
+
 // getYoutubeLink('OdVvBRNAWAI').then(console.log)
 
 
 // getYoutubeLink('VuNIsY6JdUw').then(console.log)
+reportLog('VuNIsY6JdUw',"11","Vuong","Khong xem duoc","vuong@gmail.com")
+.then(console.log)
+.then(res => getYoutubeLink('ndwGVyEIiqE'))
+.then(console.log)
 
-getYoutubeLink('ndwGVyEIiqE').then(console.log)
+// getYoutubeLink('ndwGVyEIiqE').then(console.log)
